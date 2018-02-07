b7b8253ccf9968715a8a4e1f3e0608dcaf91c338
alexbrozovich <alexbrozovich@gmail.com>
2018-02-07 11:31:01 -0500

    Initial commit

---
+++ src/MidtermProjectMainApp.java
@@ -0,0 +1,12 @@
+
+//@author Ronald Kim
+//@author Jordan Zwart
+//@author Alex Brozovich
+public class MidtermProjectMainApp {
+
+	public static void main(String[] args) {
+		System.out.println("insert minesweeper code here");
+
+	}
+
+}
