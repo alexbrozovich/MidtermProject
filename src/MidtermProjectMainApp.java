5d2759800a2512d6aa5cace7dafa6677603c1bc6
jordanzwart <ZWART.JORDAN20@GMAIL.COM>
2018-02-07 15:34:00 -0500

    Starting of Main app

--- src/MidtermProjectMainApp.java
+++ src/MidtermProjectMainApp.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 //@author Ronald Kim
 //@author Jordan Zwart
@@ -5,9 +6,23 @@
 public class MidtermProjectMainApp {
 
 	public static void main(String[] args) {
-		System.out.println("insert minesweeper code here");
+		Scanner scan = new Scanner(System.in);
 		
-		System.out.println("Hi.");
+		System.out.println("Welcome to minefield\n");
+		
+		System.out.println("What is your name?: ");
+		String name = scan.next();
+		System.out.println("What is your skill?: ");
+		String skill = scan.next();
+		Player p1 = new Player(name,skill);
+		
+		System.out.println("Hey " + name + ". Welcome to minefield)");
+		
+		
+		int size = Validator.getInt(scan, "First things first, please enter the size of board you would like to play");
+		System.out.println("Your board is " + size + " x " + size + ".");
+		
+	
 
 	}
 
